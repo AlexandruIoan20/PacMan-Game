c44dc460836a475e31a3248eba952ea3f94edc55
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2025-02-18 15:04:07 +0200

    From file to game matrix

--- app/src/GamePanel.java
+++ app/src/GamePanel.java
@@ -1,17 +1,64 @@
+import game_constants.Constants;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import game_constants.Constants;
 
 public class GamePanel extends JPanel {
     private final String filePath;
+    private final int N = Constants.N.getValue();
+    int game_matrix[][] = new int[N][N];
 
     GamePanel(String filePath) {
         this.filePath = filePath;
         this.setBackground(Color.BLACK);
         this.setOpaque(true);
+
+        this.loadLevel();
+        this.showGame();
     }
 
     private void loadLevel() {
+        try {
+            File file = new File(filePath);
 
+            FileReader fileReader = new FileReader(file);
+            System.out.println("Game file readed");
+
+            int character, line_counter = 0, e_counter = 0;
+            while((character = fileReader.read()) != -1) {
+                // Convert from read int to real int
+                char c = (char) character;
+
+                if(Character.getNumericValue(c) != -1) {
+                    game_matrix[line_counter][e_counter] = Character.getNumericValue(c);
+
+                    e_counter++;
+                    if (e_counter == N) {
+                        e_counter = 0;
+                        line_counter++;
+                    }
+                }
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch(IOException e) {
+            System.out.println("The program was unable to read the file");
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException");
+        }
+    }
+
+    private void showGame() {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                System.out.print(game_matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- app/src/Main.java
+++ app/src/Main.java
@@ -3,11 +3,5 @@ import game_constants.Constants;
 public class Main {
     public static void main(String[] args) {
         GameFrame gameFrame = new GameFrame();
-
-        int squareSize = Constants.SQUARE_SIZE.getValue();
-        int N = Constants.N.getValue();
-
-        System.out.println(squareSize);
-        System.out.println(N);
     }
 }
